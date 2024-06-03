b4f93c74d98545fc2ee9871cb8eac924b5f521c8
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-06-02 23:12:17 -0300

    adição nomes membros

--- paginas/membros.php
+++ paginas/membros.php
@@ -12,11 +12,17 @@
                   <i class="fa-brands fa-github" style="color:black"></i>
                 </a>
               </div>
-              <div class="col-10" style="background-color:#A68064; padding: 16px; color: aliceblue;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam obcaecati exercitationem aperiam ratione, necessitatibus beatae totam assumenda, consequatur, temporibus quod consectetur inventore nemo esse explicabo error doloremque alias numquam officiis.</div>
+              <div class="col-10" style="background-color:#A68064; padding: 16px; color: aliceblue;"><h2>Marcos Vinicius Bartoli Senko</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ullam obcaecati exercitationem aperiam ratione, necessitatibus beatae totam assumenda, consequatur, temporibus quod consectetur inventore nemo esse 
+              explicabo error doloremque alias numquam officiis.</p></div>
             </div>
 
             <div class="row">
-              <div class="col-10" style="background-color: #5C3317; padding: 16px; color: aliceblue;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolores quam ipsam quia perferendis eius autem commodi blanditiis nesciunt cum fugit ex temporibus facere nam est ad asperiores, minima deserunt.</div>
+              <div class="col-10" style="background-color: #5C3317; padding: 16px; color: aliceblue;"><h2>André Roberto Azevedo</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Tenetur dolores quam ipsam quia perferendis eius autem commodi blanditiis nesciunt cum fugit ex temporibus facere nam est ad asperiores, minima deserunt.
+              </p></div>
               <div class="col-2" style="background-color: aliceblue; padding: 16px;">
                 <img src="img/andre.jpg" alt="Andre" title="Andre" style="width: 100%;">
                 <a href="https://github.com/Azvedo-06">
@@ -32,11 +38,16 @@
                   <i class="fa-brands fa-github" style="color:black"></i>
                 </a>
               </div>
-              <div class="col-10" style="background-color: #A68064; padding: 16px; color: aliceblue;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti eveniet eos suscipit fugiat ipsam. Earum eveniet numquam, impedit doloribus sit deserunt quos totam esse nisi distinctio soluta dicta incidunt eligendi.</div>
+              <div class="col-10" style="background-color: #A68064; padding: 16px; color: aliceblue;"><h2></h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+               Corrupti eveniet eos suscipit fugiat ipsam. Earum eveniet numquam, impedit doloribus sit deserunt quos 
+               totam esse nisi distinctio soluta dicta incidunt eligendi.</p></div>
             </div>
 
             <div class="row">
-              <div class="col-10" style="background-color: #5C3317; padding: 16px; color: aliceblue;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas pariatur facere accusamus explicabo culpa dolor ex enim dolorem saepe, molestiae voluptatem, nihil dolore libero reprehenderit cum doloribus ipsa ea possimus!</div>
+              <div class="col-10" style="background-color: #5C3317; padding: 16px; color: aliceblue;"><h2>Guilherme da Silva Freitas</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas pariatur facere accusamus explicabo culpa dolor ex enim dolorem saepe, 
+              molestiae voluptatem, nihil dolore libero reprehenderit cum doloribus ipsa ea possimus!</p></div>
               <div class="col-2" style="background-color: aliceblue; padding: 16px;">
                 <img src="img/guilherme.jpg" alt="Guilherme" title="Guilherme" style="width: 100%;">
                 <a href="">
