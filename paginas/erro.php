c26d3298f013da95d0d7dd76b2c332292ba541e0
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-06-03 21:14:37 -0300

    adição de uma imagem na pagina erro

--- paginas/erro.php
+++ paginas/erro.php
@@ -2,6 +2,7 @@
     <h1 style="padding: 60px;">404 - Página não encontrada</h1>
     <h2 style="padding-bottom: 10px;">Ops! Algo deu errado.</h2>
     <p style="padding-bottom: 30px;">A página que você está procurando não existe ou foi movida.</p>
+    <img src="img/error404.png">
 
     <h3>Revise seu trajeto e encaminhe o problema para nosso suporte</h3>
     <a href="index.php?pagina=suporte">Suporte</a><br>
