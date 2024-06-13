016a0059279e5a99171ad1be1167c1de8eea7416
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-06-13 15:00:34 -0300

    reponsividade na pagina membros

--- paginas/membros.php
+++ paginas/membros.php
@@ -1,104 +1,125 @@
 <main>
-          <div class="jogos" style="background-color: #411b01;">
-            <h2><strong>Membros: </strong></h2>
-        </div>
-    
-        <section class="membros">
-            <div class="row">
-              <div class="col-2" style="background-color: aliceblue; padding: 16px;">
-              <img src="img/vinii.png" alt="Marcos Vinicius Bartoli Senko" title="Marcos Vinicius" style="width: 100%; padding: 8px;">
-              <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
-                <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-              </div>
-              <div class="col-10" style="background-color:#A68064; padding: 16px; color: aliceblue;"><h2>Marcos Vinicius Bartoli Senko</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Ullam obcaecati exercitationem aperiam ratione, necessitatibus beatae totam assumenda, consequatur, temporibus quod consectetur inventore nemo esse 
-              explicabo error doloremque alias numquam officiis.</p></div>
-            </div>
+  <div class="jogos" style="background-color: #411b01;">
+    <h2><strong>Membros: </strong></h2>
+  </div>
 
-            <div class="row">
-              <div class="col-10" style="background-color: #411b01; padding: 16px; color: aliceblue;"><h2>André Roberto Azevedo</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Tenetur dolores quam ipsam quia perferendis eius autem commodi blanditiis nesciunt cum fugit ex temporibus facere nam est ad asperiores, minima deserunt.
-              </p></div>
-              <div class="col-2" style="background-color: aliceblue; padding: 16px;">
-                <img src="img/andre.jpg" alt="Andre" title="Andre" style="width: 100%; padding: 8px;">
-                <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
-                <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-              </div>
-            </div>
+  <section class="membros">
+    <div class="row">
+      <div class="col-2" style="background-color: aliceblue; padding: 16px;">
+        <img src="img/vinii.png" alt="Marcos Vinicius Bartoli Senko" title="Marcos Vinicius" style="width: 100%; padding: 8px;">
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+      <div class="col-10" style="background-color:#A68064; padding: 16px; color: aliceblue;">
+        <h2>Marcos Vinicius Bartoli Senko</h2>
+        <p>Marcos, desenvolvedor do site MVGames e criador do jogo SwampChalleng, tem 20 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+          Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Marcos estuda no Centro Universitário Integrado.</p>
+      </div>
+    </div>
 
-            <div class="row">
-              <div class="col-2" style="background-color: aliceblue; padding: 16px;">
-                <img src="img/felipe.jpg" alt="Felipe" title="Felipe" style="width: 100%; padding: 8px;">
-                <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
-                <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-              </div>
-              <div class="col-10" style="background-color: #A68064; padding: 16px; color: aliceblue;"><h2>Felipe Alves de Morais</h2>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-               Corrupti eveniet eos suscipit fugiat ipsam. Earum eveniet numquam, impedit doloribus sit deserunt quos 
-               totam esse nisi distinctio soluta dicta incidunt eligendi.</p></div>
-            </div>
+    <div class="row">
+      <div class="col-10" style="background-color: #411b01; padding: 16px; color: aliceblue;">
+        <h2>André Roberto Azevedo</h2>
+        <p>André, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+        Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. André estuda no Centro Universitário Integrado.
+        </p>
+      </div>
+      <div class="col-2" style="background-color: aliceblue; padding: 16px;">
+        <img src="img/andre.jpg" alt="Andre" title="Andre" style="width: 100%; padding: 8px;">
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+    </div>
 
-            <div class="row">
-              <div class="col-10" style="background-color: #411b01; padding: 16px; color: aliceblue;"><h2>Guilherme da Silva Freitas</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas pariatur facere accusamus explicabo culpa dolor ex enim dolorem saepe, 
-              molestiae voluptatem, nihil dolore libero reprehenderit cum doloribus ipsa ea possimus!</p></div>
-              <div class="col-2" style="background-color: aliceblue; padding: 16px;">
-                <img src="img/guilherme.jpg" alt="Guilherme" title="Guilherme" style="width: 100%; padding: 8px;">
-                <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
-                <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-              </div>
-            </div>
-        </section>
+    <div class="row">
+      <div class="col-2" style="background-color: aliceblue; padding: 16px;">
+        <img src="img/felipe.jpg" alt="Felipe" title="Felipe" style="width: 100%; padding: 8px;">
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+      <div class="col-10" style="background-color: #A68064; padding: 16px; color: aliceblue;">
+        <h2>Felipe Alves de Morais</h2>
+        <p>Felipe, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+        Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Felipe estuda no Centro Universitário Integrado.</p>
+      </div>
+    </div>
 
-        <section class="membros-mobile" style="flex-direction: column; align-items: center; gap: 32px;">
+    <div class="row">
+      <div class="col-10" style="background-color: #411b01; padding: 16px; color: aliceblue;">
+        <h2>Guilherme da Silva Freitas</h2>
+        <p>Guilherme, desenvolvedor do site ... e criador do jogo ..., tem 18 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+        Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Guilherme estuda no Centro Universitário Integrado.</p>
+      </div>
+      <div class="col-2" style="background-color: aliceblue; padding: 16px;">
+        <img src="img/guilherme.jpg" alt="Guilherme" title="Guilherme" style="width: 100%; padding: 8px;">
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+    </div>
+  </section>
 
-        <div class="card" style="width: 18rem;">
-            <img src="img/vinii.png" class="card-img-top" alt="Marcos Vinicius Bartoli Senko">
-          <div class="card-body" style="background-color: #A68064; color: white;">
-              <h3>Marcos Vinicius Bartoli Senko</h3>
-             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate veniam vitae porro, dolore saepe magni dolorum, 
-              natus praesentium rem eius maiores fuga enim aliquid. Exercitationem labore vero magnam iusto asperiores?</p>
-              <button style="background-color: #32CD32; padding: 8px; border-radius: 8px;">Ir para o jogo</button>
-              <button style="background-color: aliceblue; padding: 8px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-          </div>
-        </div>
 
-        <div class="card" style="width: 18rem;">
-            <img src="img/andre.jpg" class="card-img-top" alt="André Roberto Azevedo">
-          <div class="card-body" style="background-color: #5C3317; color: white;">
-              <h3>André Roberto Azevedo</h3>
-             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo aliquid dignissimos odit molestias molestiae illo eaque 
-              temporibus excepturi? Dolor maxime officia neque! Sapiente alias dignissimos qui eveniet ullam voluptatum!</p>
-              <button style="background-color: #32CD32; padding: 8px; border-radius: 8px;">Ir para o jogo</button>
-              <button style="background-color: aliceblue; padding: 8px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-          </div>
-        </div>
 
-        <div class="card" style="width: 18rem;">
-            <img src="img/felipe.jpg" class="card-img-top" alt="Felipe Alves de Morais">
-          <div class="card-body" style="background-color: #A68064; color: white;">
-              <h3>Felipe Alves de Morais</h3>
-             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente saepe ipsa consequatur, a aperiam eos consequuntur ipsum laborum, 
-              quia numquam accusamus quod obcaecati aliquam, omnis officiis molestias voluptate quam voluptas?</p>
-              <button style="background-color: #32CD32; padding: 8px; border-radius: 8px;">Ir para o jogo</button>
-              <button style="background-color: aliceblue; padding: 8px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-          </div>
-        </div>
 
-        <div class="card" style="width: 18rem;">
-            <img src="img/guilherme.jpg" class="card-img-top" alt="Guilherme da Silva Freitas">
-          <div class="card-body" style="background-color: #5C3317; color: white;">
-              <h3>Guilherme da Silva Freitas</h3>
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laudantium amet quas iste nobis magnam ad corporis minima natus. 
-              Est in totam ratione possimus nobis magni, enim minima adipisci suscipit.</p>
-              <button style="background-color: #32CD32; padding: 8px; border-radius: 8px;">Ir para o jogo</button>
-              <button style="background-color: aliceblue; padding: 8px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-          </div>
-        </div>
 
-        </section>
+
+
+
+
+  <section class="container d-flex flex-wrap justify-content-center gap-4 mt-4">
+
+    <div class="card" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=flappy">
+          <img style="border-radius: 16px;" class="w-100" src="img/vinii.png" alt="Marcos Vinicius" title="Marcos Vinicius">
+        </a>
+        <h5 style="color: white; margin: 5px;">Marcos Vinicius Bartoli Senko</h3>
+        <p style="color: #fff;">Marcos, desenvolvedor do site MVGames e criador do jogo SwampChalleng, tem 20 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+        Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Marcos estuda no Centro Universitário Integrado.</p>
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=swamp">
+          <img style="border-radius: 16px;" class="w-100" src="img/andre.jpg" alt="André Roberto" title="André Roberto">
+        </a>
+        <h5 style="color: white; margin: 5px;">André Roberto Azevedo</h3>
+        <p style="color: #fff;">André, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+        Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. André estuda no Centro Universitário Integrado.</p>
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=dangeon">
+          <img style="border-radius: 16px;" class="w-100" src="img/felipe.jpg" alt="Felipe Alves" title="Felipe Alves">
+        </a>
+        <h5 style="color: white; margin: 5px;">Felipe Alves de Morais</h3>
+        <p style="color: #fff;">Felipe, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+        Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Felipe estuda no Centro Universitário Integrado.</p>
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=galactic">
+          <img style="border-radius: 16px;" class="w-100" src="img/guilherme.jpg" alt="Guilherme da Silva" title="Guilherme da Silva">
+        </a>
+        <h5 style="color: white; margin: 5px;">Guilherme da Silva Freitas</h3>
+        <p style="color: #fff;">Guilherme, desenvolvedor do site ... e criador do jogo ..., tem 18 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e 
+        Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Guilherme estuda no Centro Universitário Integrado.</p>
+        <button style="background-color: #32CD32; padding: 4px; border-radius: 8px;">Ir para o jogo</button>
+        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+      </div>
+    </div>
+
+    </div>
+  </section>
 </main>
-
-   
