998f04edb112e5662c3acb69a8fa4e07584c5103
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-06-13 00:00:06 -0300

    estilizando o 'conheça também'

--- paginas/dangeon.php
+++ paginas/dangeon.php
@@ -1,6 +1,6 @@
 <main class="dangeon">
 <div class="img-ind" style="width: 100%; text-align: center; padding: 32px;">
-        <img style=" border: 8px solid #fff; border-radius: 16px;" src="img/dangeonsurvival.jpg" alt="SwampChallenge" title="SwampChallenge">
+        <img style=" border: 4px solid #FF6600; border-radius: 16px;" src="img/dangeonsurvival.jpg" alt="SwampChallenge" title="SwampChallenge">
     </div>
     <h3 style="text-align: center; color: white;">SwampChallenge</h3>
 
@@ -17,20 +17,29 @@
         <a href="#" class="btn-jogar" style="background-color: #00CC00; padding: 8px; text-decoration: none; border-radius: 8px; color: white;">Jogar agora</a>
     </div>
 
-    <p style="color: white; padding-top: 80px; margin: 40px;">Conheça também:</p>
+    <div class="conheca-tambem">
+        <p><strong>Conheça também:</strong></p>
+    </div>
 
 
-    <div class="conheca" style="justify-content: flex-start; margin: 8px ;padding: 32px;">
-        <a href="index.php?pagina=flappy">
-            <img style="width: 200px; border-radius: 16px;" src="img/flappy.jpg" alt="FlappyBurnes" title="FlappyBurnes">
-        </a>
+    <div class="card-conheca" style="justify-content: flex-start; margin: 8px ;padding: 32px;">
 
-        <a href="index.php?pagina=dangeon">
-            <img style="width: 200px; border-radius: 16px;" src="img/swamp.jpg" alt="SwampChallenge" title="SwampChallenge">
-        </a>
+        <div>
+            <a href="index.php?pagina=flappy">
+                <img class="conheca" style="width: 200px; border-radius: 16px;" src="img/flappy.jpg" alt="FlappyBurnes" title="FlappyBurnes">
+            </a>
+        </div>
 
-        <a href="index.php?pagina=galactic">
-            <img style="width: 200px; border-radius: 16px;" src="img/galactic.jpg" alt="Galactic Annahillaion" title="Galactic Annahillaion">
-        </a>
+        <div>
+            <a href="index.php?pagina=swamp">
+                <img class="conheca" style="width: 200px; border-radius: 16px;" src="img/dangeonsurvival.jpg" alt="SwampChallenge" title="SwampChallenge">
+            </a>
+        </div>
+
+        <div>
+            <a href="index.php?pagina=galactic">
+                <img class="conheca" style="width: 200px; border-radius: 16px;" src="img/galactic.jpg" alt="Galactic Annahillaion" title="Galactic Annahillaion">
+            </a>
+        </div>
     </div>
 </main>
--- paginas/galactic.php
+++ paginas/galactic.php
@@ -1,13 +1,13 @@
 <main class="galactic">
-<div class="img-ind" style="width: 100%; text-align: center; padding: 32px;">
-        <img style=" border: 8px solid #fff; border-radius: 16px;" src="img/galactic.jpg" alt="SwampChallenge" title="SwampChallenge">
+    <div class="img-ind" style="width: 100%; text-align: center; padding: 32px;">
+        <img style=" border: 4px solid #FF6600; border-radius: 16px;" src="img/galactic.jpg" alt="SwampChallenge" title="SwampChallenge">
     </div>
     <h3 style="text-align: center; color: white;">Galactic Annahillaion</h3>
 
 
     <div style="margin-right: 300px; margin-left: 300px;">
-    <p style="color: white;">Galactic annihilation é um jogo de ação e aventura em que o jogador assume o controle de um piloto habilidoso em uma
-        missão para destruir inimigos que ameaçam a paz no espaço sideral. O objetivo principal do jogo é eliminar todas as ameaças espaciais e completar o game com sucesso</p>
+        <p style="color: white;">Galactic annihilation é um jogo de ação e aventura em que o jogador assume o controle de um piloto habilidoso em uma
+            missão para destruir inimigos que ameaçam a paz no espaço sideral. O objetivo principal do jogo é eliminar todas as ameaças espaciais e completar o game com sucesso</p>
         <p style="color: white;">Desenvolvedor: Guilherme da Silva Freitas</p>
         <p style="color: white;">Ano: 2024</p>
         <p style="color: white;">Categoria: Aventura</p>
@@ -17,20 +17,29 @@
         <a href="#" class="btn-jogar" style="background-color: #00CC00; padding: 8px; text-decoration: none; border-radius: 8px; color: white;">Jogar agora</a>
     </div>
 
-    <p style="color: white; padding-top: 80px; margin: 40px;">Conheça também:</p>
+    <div class="conheca-tambem">
+        <p><strong>Conheça também:</strong></p>
+    </div>
 
 
-    <div class="conheca" style="justify-content: flex-start; margin: 8px ;padding: 32px;">
-        <a href="index.php?pagina=flappy">
-            <img style="width: 200px; border-radius: 16px;" src="img/flappy.jpg" alt="FlappyBurnes" title="FlappyBurnes">
-        </a>
+    <div class="card-conheca" style="justify-content: flex-start; margin: 8px ;padding: 32px;">
 
-        <a href="index.php?pagina=dangeon">
-            <img style="width: 200px; border-radius: 16px;" src="img/dangeonsurvival.jpg" alt="DangeonSurvival" title="DangeonSurvival">
-        </a>
+        <div>
+            <a href="index.php?pagina=flappy">
+                <img class="conheca" style="width: 200px; border-radius: 16px;" src="img/flappy.jpg" alt="FlappyBurnes" title="FlappyBurnes">
+            </a>
+        </div>
 
-        <a href="index.php?pagina=galactic">
-            <img style="width: 200px; border-radius: 16px;" src="img/Swamp.jpg" alt="SwampChallenge" title="SwampChallenge">
-        </a>
+        <div>
+            <a href="index.php?pagina=dangeon">
+                <img class="conheca" style="width: 200px; border-radius: 16px;" src="img/dangeonsurvival.jpg" alt="DangeonSurvival" title="DangeonSurvival">
+            </a>
+        </div>
+
+        <div>
+            <a href="index.php?pagina=swamp">
+                <img class="conheca" style="width: 200px; border-radius: 16px;" src="img/swamp.jpg" alt="SwampChallenge" title="SwampChallenge">
+            </a>
+        </div>
     </div>
 </main>
