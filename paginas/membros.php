8fe7b0bfd939fd96c6fed0f5d37f4fc0eb535c61
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-06-18 21:21:39 -0300

    add pasta dos jogos

--- paginas/membros.php
+++ paginas/membros.php
@@ -10,7 +10,11 @@
         <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="index.php?pagina=swamp">
           Ir para o Jogo
         </a>
-        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+
+        <a href="https://github.com/Marcosvini0001" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+          <i class="fa-brands fa-github" style="color:black"></i>GitHub
+        </a>
+
       </div>
       <div class="col-10" style="background-color:#A68064; padding: 16px; color: aliceblue;">
         <h2>Marcos Vinicius Bartoli Senko</h2>
@@ -31,7 +35,9 @@
         <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="index.php?pagina=dangeon">
           Ir para o Jogo
         </a>
-        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+        <a href="https://github.com/Azvedo-06" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+          <i class="fa-brands fa-github" style="color:black"></i>GitHub
+        </a>
       </div>
     </div>
 
@@ -41,7 +47,9 @@
         <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="index.php?pagina=flappy">
           Ir para o Jogo
         </a>
-        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+        <a href="https://github.com/FelipeTr00/TechAcademy2/" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+          <i class="fa-brands fa-github" style="color:black"></i>GitHub
+        </a>
       </div>
       <div class="col-10" style="background-color: #A68064; padding: 16px; color: aliceblue;">
         <h2>Felipe Alves de Morais</h2>
@@ -61,7 +69,9 @@
         <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="index.php?pagina=galactic">
           Ir para o Jogo
         </a>
-        <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
+        <a href="https://github.com/guilhermefreitas450/" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+          <i class="fa-brands fa-github" style="color:black"></i>GitHub
+        </a>
       </div>
     </div>
   </section>
@@ -72,69 +82,79 @@
 
 
 
-<!----------------------------------------------- RESPONSIVIDADE AQUI ABAIXO - PROBLEMA 2 -------------------------------------------->
+  <!----------------------------------------------- RESPONSIVIDADE AQUI ABAIXO - PROBLEMA 2 -------------------------------------------->
 
   <section class="container d-flex flex-wrap justify-content-center gap-4 mt-4">
 
-      <div class="card-membros" style="width: 18rem; border: 5px solid black;">
-        <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
-          <a href="index.php?pagina=swamp">
-            <img style="border-radius: 16px;" class="w-100" src="img/vinii.png" alt="Marcos Vinicius" title="Marcos Vinicius">
+    <div class="card-membros" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=swamp">
+          <img style="border-radius: 16px;" class="w-100" src="img/vinii.png" alt="Marcos Vinicius" title="Marcos Vinicius">
+        </a>
+        <h5 style="color: white; margin: 5px;">Marcos Vinicius Bartoli Senko</h3>
+          <p style="color: #fff;">Marcos, desenvolvedor do site MVGames e criador do jogo SwampChalleng, tem 20 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
+            Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Marcos estuda no Centro Universitário Integrado.</p>
+          <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
+            Ir para o Jogo
           </a>
-          <h5 style="color: white; margin: 5px;">Marcos Vinicius Bartoli Senko</h3>
-            <p style="color: #fff;">Marcos, desenvolvedor do site MVGames e criador do jogo SwampChalleng, tem 20 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
-              Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Marcos estuda no Centro Universitário Integrado.</p>
-            <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
-              Ir para o Jogo
-            </a>
-            <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-        </div>
-      </div>
 
-      <div class="card-membros" style="width: 18rem; border: 5px solid black;">
-        <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
-          <a href="index.php?pagina=dangeon">
-            <img style="border-radius: 16px;" class="w-100" src="img/andre.jpg" alt="André Roberto" title="André Roberto">
+          <a href="https://github.com/Marcosvini0001" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+            <i class="fa-brands fa-github" style="color:black"></i>GitHub
           </a>
-          <h5 style="color: white; margin: 5px;">André Roberto Azevedo</h3>
-            <p style="color: #fff;">André, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
-              Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. André estuda no Centro Universitário Integrado.</p>
-            <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
-              Ir para o Jogo
-            </a>
-            <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-        </div>
-      </div>
 
-      <div class="card-membros" style="width: 18rem; border: 5px solid black;">
-        <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
-          <a href="index.php?pagina=flappy">
-            <img style="border-radius: 16px;" class="w-100" src="img/felipe.jpg" alt="Felipe Alves" title="Felipe Alves">
-          </a>
-          <h5 style="color: white; margin: 5px;">Felipe Alves de Morais</h3>
-            <p style="color: #fff;">Felipe, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
-              Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Felipe estuda no Centro Universitário Integrado.</p>
-            <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
-              Ir para o Jogo
-            </a>
-            <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-        </div>
       </div>
+    </div>
 
-      <div class="card-membros" style="width: 18rem; border: 5px solid black;">
-        <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
-          <a href="index.php?pagina=galactic">
-            <img style="border-radius: 16px;" class="w-100" src="img/guilherme.jpg" alt="Guilherme da Silva" title="Guilherme da Silva">
+    <div class="card-membros" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=dangeon">
+          <img style="border-radius: 16px;" class="w-100" src="img/andre.jpg" alt="André Roberto" title="André Roberto">
+        </a>
+        <h5 style="color: white; margin: 5px;">André Roberto Azevedo</h3>
+          <p style="color: #fff;">André, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
+            Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. André estuda no Centro Universitário Integrado.</p>
+          <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
+            Ir para o Jogo
           </a>
-          <h5 style="color: white; margin: 5px;">Guilherme da Silva Freitas</h3>
-            <p style="color: #fff;">Guilherme, desenvolvedor do site ... e criador do jogo ..., tem 18 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
-              Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Guilherme estuda no Centro Universitário Integrado.</p>
-            <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
-              Ir para o Jogo
-            </a>
-            <button style="background-color: aliceblue; padding: 4px; border-radius: 8px;"><i class="fa-brands fa-github" style="color:black"></i>GitHub</button>
-        </div>
+          <a href="https://github.com/Azvedo-06" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+          <i class="fa-brands fa-github" style="color:black"></i>GitHub
+        </a>
       </div>
+    </div>
+
+    <div class="card-membros" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=flappy">
+          <img style="border-radius: 16px;" class="w-100" src="img/felipe.jpg" alt="Felipe Alves" title="Felipe Alves">
+        </a>
+        <h5 style="color: white; margin: 5px;">Felipe Alves de Morais</h3>
+          <p style="color: #fff;">Felipe, desenvolvedor do site ... e criador do jogo ..., tem .. anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
+            Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Felipe estuda no Centro Universitário Integrado.</p>
+          <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
+            Ir para o Jogo
+          </a>
+          <a href="https://github.com/FelipeTr00/TechAcademy2/" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+          <i class="fa-brands fa-github" style="color:black"></i>GitHub
+        </a>
+      </div>
+    </div>
+
+    <div class="card-membros" style="width: 18rem; border: 5px solid black;">
+      <div style="background-color: black;" class="card-body d-flex flex-column align-items-center">
+        <a href="index.php?pagina=galactic">
+          <img style="border-radius: 16px;" class="w-100" src="img/guilherme.jpg" alt="Guilherme da Silva" title="Guilherme da Silva">
+        </a>
+        <h5 style="color: white; margin: 5px;">Guilherme da Silva Freitas</h3>
+          <p style="color: #fff;">Guilherme, desenvolvedor do site ... e criador do jogo ..., tem 18 anos. Ele é estudante do primeiro período do curso de Tecnologia em Análise e
+            Desenvolvimento de Sistemas (TADS) e mora em Campo Mourão, PR. Guilherme estuda no Centro Universitário Integrado.</p>
+          <a style="background-color: #32CD32; padding: 4px; border-radius: 8px; text-decoration: none; color: white;" href="">
+            Ir para o Jogo
+          </a>
+          <a href="https://github.com/guilhermefreitas450/" style="background-color: aliceblue; padding: 4px; border-radius: 8px; text-decoration: none; color: black;">
+          <i class="fa-brands fa-github" style="color:black"></i>GitHub
+        </a>
+      </div>
+    </div>
     </div>
 
   </section>
