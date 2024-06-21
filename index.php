2a3af03b21608c43c6842f82cea436fa1eab7b99
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-06-21 13:24:17 -0300

    media para .jogo-responsive

--- index.php
+++ index.php
@@ -1,89 +1,92 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <base href="http://localhost/techacademy2php/">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/b3b83db5db.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="shortcut icon" href="img/mvz.png">
-    <title>MVGames</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/b3b83db5db.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/aos.css">
+  <link rel="shortcut icon" href="img/mvz.png">
+  <link rel="stylesheet" href="css/style.css">
+  <title>MVGames</title>
 </head>
+
 <body class="body">
-    <header class="header" >
-        <nav class="navbar navbar-expand-lg" style="background-color: #9c3f00;" data-bs-theme="dark">
-            <div class="container-fluid">
-              <a href="index.php?pagina=home">
-                <img src="img/mvz.png" alt="MVGames" title="MVGames" style="width: 80px;" alt="MV">
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php?pagina=home">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="index.php?pagina=membros">Membros</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="index.php?pagina=suporte">Suporte</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>    
-    </header>
-
-    <main>
-        <?php
-            //imprimir o conteudo do array do GET
-            //print_r($_GET);
-
-            //recuperar a variavel pagina
-            $pagina = $_GET["pagina"] ?? "home";
-
-            // paginas/home.php
-            $pagina = "paginas/{$pagina}.php";
-
-            //verificar se o arquivo existe
-            if (file_exists($pagina)) {
-                include $pagina;
-            } else {
-                include "paginas/erro.php";
-            }
-
-        ?>
-    </main>
-    
-
-    <footer class="footer"> 
-      <div class="pfooter">
-        <p class="paragrafofooter">Desenvolvido por:<br>Marcos Vinicius<br>BS &copy;</p>
+  <header class="header">
+    <nav class="navbar navbar-expand-lg" style="background-color: #9c3f00;" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a href="index.php?pagina=home">
+          <img src="img/mvz.png" alt="MVGames" title="MVGames" style="width: 80px;" alt="MV">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php?pagina=home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?pagina=membros">Membros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?pagina=suporte">Suporte</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <a href="index.php?pagina=home">
+    </nav>
+  </header>
+
+  <main>
+    <?php
+    //imprimir o conteudo do array do GET
+    //print_r($_GET);
+
+    //recuperar a variavel pagina
+    $pagina = $_GET["pagina"] ?? "home";
+
+    // paginas/home.php
+    $pagina = "paginas/{$pagina}.php";
+
+    //verificar se o arquivo existe
+    if (file_exists($pagina)) {
+      include $pagina;
+    } else {
+      include "paginas/erro.php";
+    }
+
+    ?>
+  </main>
+
+
+  <footer class="footer">
+    <div class="pfooter">
+      <p class="paragrafofooter">Desenvolvido por:<br>Marcos Vinicius<br>BS &copy;</p>
+    </div>
+    <a href="index.php?pagina=home">
       <img class="logofooter" src="img/mvz.png" alt="MVGames" title="MVGames">
-      </a>
-      <div class="d-flex gap-4">
-        <a class="listagemfooter" href="index.php?pagina=home">Home</a>
-        <a class="listagemfooter" href="index.php?pagina=membros">Membros</a>
-        <a class="listagemfooter" href="index.php?pagina=suporte">Suporte</a>
-      </div>
-    </footer>
+    </a>
+    <div class="d-flex gap-4">
+      <a class="listagemfooter" href="index.php?pagina=home">Home</a>
+      <a class="listagemfooter" href="index.php?pagina=membros">Membros</a>
+      <a class="listagemfooter" href="index.php?pagina=suporte">Suporte</a>
+    </div>
+  </footer>
 
 
-    <script src="js/aos.js"></script>
-    <script src="js/fslightbox.js"></script>
-    <script src="https://kit.fontawesome.com/b3b83db5db.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/fslightbox.js"></script>
+  <script src="https://kit.fontawesome.com/b3b83db5db.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
